879bcf0edf56182fa93331cc5bf2d4b11b3a3edc
Abhijot singh <manjeetkaur2w00786@gmail.com>
2024-11-17 03:54:17 -0800

    initffinidnnrionngbgbnbg

---
+++ exam eligibility.py
@@ -0,0 +1,7 @@
+workingdayattendence=int(input("enter no. to check"))
+if workingdayattendence>=200:
+    print ("your eligible for examination")
+elif workingdayattendence==195:
+          print("just contact on 94563-56788 if you want to participate in examination")
+else:
+         print("you are not eligible for examination")
